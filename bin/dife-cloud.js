a89b86463f3b8f71438b37438ddf2489a55908a5
Seungho Lee <poream3387@gmail.com>
2024-04-14 17:52:00 +0900

    ci(rest-api): REST API 용 서버의 자동배포 필요 (#11)
    개요
    
    - REST API의 자동 배포를 위해, 필요한 리소스를 구성하자
    
    수정 사항
    
    - ApplicationStack
      - 어플리케이션용 EC2 배포
      - SSH와 8080 포트 접근만 허용
      - 외부에서도 요청을 호출해야하므로 public으로 한다
      - EC2를 처음 돌리면, docker와 aws-cli가 부재하는데, 이것을 자동화
    스크립트를 넣어서 초기에 사용할 수 있도록 하자
      - production 태그 사용
    
    - PipelineStack
      - 도커 이미지 저장용 ECR을 배포

--- bin/dife-cloud.js
+++ bin/dife-cloud.js
@@ -4,6 +4,8 @@ const cdk = require("aws-cdk-lib");
 const { NetworkStack } = require("../lib/dife-network-stack");
 const { DatabaseStack } = require("../lib/dife-database-stack");
 const { StorageStack } = require("../lib/dife-storage-stack");
+const { ApplicationStack } = require("../lib/dife-application-stack");
+const { PipelineStack } = require("../lib/dife-pipeline-stack");
 
 const app = new cdk.App();
 
@@ -14,3 +16,9 @@ const databaseStack = new DatabaseStack(app, "DifeDatabaseStack", {
 });
 
 const storageStack = new StorageStack(app, "DifeStorageStack");
+
+const applicationStack = new ApplicationStack(app, "DifeApplicationStack", {
+    vpc: networkingStack.vpc,
+});
+
+const pipelineStack = new PipelineStack(app, "DifePipelineStack");
