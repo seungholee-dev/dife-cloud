86ebf0c5a175df54957d89d9392dec3390015109
Seungho Lee <poream3387@gmail.com>
2024-04-14 17:49:45 +0900

    refactor(stack): Cloud Stack을 Decouple하자 (#10)
    개요
    
    - 현재 개발 중인 코드에 대해, 롤백과 배포가 전체 단위로 되므로
    관리하기가 어렵다
    
    - 따라서, 스택 별로 나누어 개발이 더 쉽게한다

--- bin/dife-cloud.js
+++ bin/dife-cloud.js
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
 
-const cdk = require('aws-cdk-lib');
-const { DifeCloudStack } = require('../lib/dife-cloud-stack');
+const cdk = require("aws-cdk-lib");
+const { NetworkStack } = require("../lib/dife-network-stack");
+const { DatabaseStack } = require("../lib/dife-database-stack");
+const { StorageStack } = require("../lib/dife-storage-stack");
 
 const app = new cdk.App();
-new DifeCloudStack(app, 'DifeCloudStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const networkingStack = new NetworkStack(app, "DifeNetworkStack");
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const databaseStack = new DatabaseStack(app, "DifeDatabaseStack", {
+    vpc: networkingStack.vpc,
 });
+
+const storageStack = new StorageStack(app, "DifeStorageStack");
