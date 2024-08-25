d29e0c84deceeb6b51d46d9e72df94ea8f2ca988
Seungho Lee <poream3387@gmail.com>
2024-08-25 19:00:56 +0900

    chore(s3): S3 접근 강화 및 versioning 활성화 (#20)
    개요
    
    - 현재 S3는 파일 링크만 있으면 누구나 파일에 access 할 수 있다.
    - 따라서, public access를 모두 없애고, pre-signed url로만 access할 수
    있도록 한다.
    - 삭제 및 수정으로부터 versioning을 활성화해서 트래킹을 할 수
    있도록 한다.
    - server side encryption을 활성화하여 데이터가 조금이라도 안전하게
    저장되도록 함

--- lib/dife-storage-stack.ts
+++ lib/dife-storage-stack.ts
@@ -1,5 +1,9 @@
 import { RemovalPolicy, Stack } from "aws-cdk-lib";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import {
+	BlockPublicAccess,
+	Bucket,
+	BucketEncryption,
+} from "aws-cdk-lib/aws-s3";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Construct } from "constructs";
 
@@ -10,13 +14,19 @@ export class StorageStack extends Stack {
 		super(scope, id, props);
 
 		const bucket = new Bucket(this, "DifeBucket", {
+			versioned: true,
 			removalPolicy: RemovalPolicy.RETAIN,
 			bucketName: "dife-bucket",
+			encryption: BucketEncryption.S3_MANAGED,
+			blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
 		});
 
 		new Bucket(this, "DifeLogsBucket", {
+			versioned: true,
 			removalPolicy: RemovalPolicy.RETAIN,
 			bucketName: "dife-logs-bucket",
+			encryption: BucketEncryption.S3_MANAGED,
+			blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
 		});
 
 		this.createParameter(
